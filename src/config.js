7ece33c289ba793ca4c3072b7bf2071971707a60
JohnnyBs <johnnybsantos0@gmail.com>
2020-05-27 16:41:28 -0300

    Alteação de modelo de hospedagem

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ global.EMAIL_TMPL = '<strong>{0}</strong>'
 
 module.exports = {
     connectionString: 'mongodb+srv://johnnybs:johnnybs@cluster0-x4mtm.mongodb.net/test?retryWrites=true&w=majority',
-    sendgridkey: 'sua chave',//Chave para enviar email
+    sendgridkey: '7c670ba4c1e0b1d8a6ab866456540961',//Chave para enviar email
     //chave para armazenar as imagens do produto do azure
-    containerConnectionString: 'sua chave'
+    containerConnectionString: 'DefaultEndpointsProtocol=https;AccountName=nodestrs;AccountKey=VBoCF0x9AkQxj+58WHH1qHYMsCsON1HOs8yeYaXTsYmzHCZq3IfzKFuNPAxvcb79CPhtq3vNK5new9N3XSwJyg==;EndpointSuffix=core.windows.net'
 }
