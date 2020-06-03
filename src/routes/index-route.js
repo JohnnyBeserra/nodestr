c3d5cacd09d9e239e88b0234be13d5a5b2e35261
JohnnyBs <johnnybsantos0@gmail.com>
2020-06-03 08:59:28 -0300

    Api finalizada

--- src/routes/index-route.js
+++ src/routes/index-route.js
@@ -7,7 +7,7 @@ const router = express.Router();
 const route = router.get('/', (req, res, next) => {
     res.status(200).send({
         title: "Node Story API",
-        version: "0.0.2"
+        version: "1.0.0"
     })
 });
 
